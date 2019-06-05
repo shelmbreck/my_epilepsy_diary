c4bfe89b3f2544b9daca603cb89623e228d9104f
shelmbreck <samantha.helmbreck@gmail.com>
2019-06-05 07:30:02 -0700

    created seizure views and medication views

---
+++ medication/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+# Create your views here.
+@api_view(['GET', 'POST'])
+def medications_index(request):
+  if request.method == 'GET':
+    medications = Medication.objects.all()
+    serializer = MedicationSerializer(seizures, many=True)
+    return Response(serializer.data)
+  elif request.method == 'POST':
+    serializer = MedicationSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return  Response(serializer.errors)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def medication_detail(request, pk):
+    medication = Medication.objects.get(pk=pk)
+
+    if request.method == 'GET':
+      serializer = MedicationSerializer(medication)
+      return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+      serializer = SeizureSerializer(seizure, data=request.data)
+      if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+      print("in here")
+      seizure.delete()
+      return Response(status=status.HTTP_204_NO_CONTENT)
---
+++ seizures/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# from .models import Dog, DogToy
+# from .serializers import serializers
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Create your views here.
+@api_view(['GET', 'POST'])
+def seizures_index(request):
+  if request.method == 'GET':
+    seizures = Seizure.objects.all()
+    serializer = SeizureSerializer(seizures, many=True)
+    return Response(serializer.data)
+  elif request.method == 'POST':
+    serializer = Seizure Serializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return  Response(serializer.errors)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def seizure_detail(request, pk):
+    seizure = Seizure.objects.get(pk=pk)
+
+    if request.method == 'GET':
+      serializer = SeizureSerializer(seizure)
+      return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+      serializer = SeizureSerializer(seizure, data=request.data)
+      if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+      print("in here")
+      seizure.delete()
+      return Response(status=status.HTTP_204_NO_CONTENT)
+
